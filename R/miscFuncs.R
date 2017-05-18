0def5eecd5a6d5abc452222b9118b2b75431d48c
johninpdx <jlight@ori.org>
2017-05-17 17:30:37 -0700

    v0.102.0 Added %w/o% operator

---
+++ R/miscFuncs.R
@@ -0,0 +1,6 @@
+#FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+#' 'Without' operator (complement of %in%):
+#' @details A binary operator, i.e. x %w/o% y (x and y vectors, or lists) gives
+#'    the elements of x that are NOT IN y.
+#' @export
+"%w/o%" <- function(x,y) x[!x %in% y] #Bin operator, x NOT IN y (vectors)

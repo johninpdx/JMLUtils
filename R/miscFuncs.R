d2081021dc4a04294d02e98e12441da949c1579b
John Light <jlight@ori-eug.ori.org>
2017-06-12 15:31:31 -0700

    v0.103.0 Adds geodesic dist and triad census stats, for use with sienaGOF Minor cleanups

--- R/miscFuncs.R
+++ R/miscFuncs.R
@@ -3,4 +3,4 @@
 #' @details A binary operator, i.e. x %w/o% y (x and y vectors, or lists) gives
 #'    the elements of x that are NOT IN y.
 #' @export
-"%w/o%" <- function(x,y) x[!x %in% y] #Bin operator, x NOT IN y (vectors)
+"%w/o%" <- function(x,y) {x[!x %in% y]} #Bin operator, x NOT IN y (vectors)

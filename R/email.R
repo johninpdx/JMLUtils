1f248a5e6968275c6987a5a7493085ed831f6a41
johninpdx <jlight@ori.org>
2020-05-09 12:11:38 -0700

    Doc added/pushed

---
+++ R/email.R
@@ -0,0 +1,51 @@
+#FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+# >> setupEmail <<
+#______________________________________________________________________________
+#' Setup auto-sending an Email after some other code runs
+#'(only works for John's Gmail account, for now. Maybe generalized
+#' someday).
+#'
+#' @return A list containing [[1]] the text message body, [[2]] credential key
+#' @export
+setupEmail <- function(){
+  #' @import blastula
+  #' @import keyring
+  #This will ask for your gmail password
+  credKey <- create_smtp_creds_key(
+    id="gmail",
+    user="johninsepdx@gmail.com",
+    provider="gmail"
+  )
+  cat("Ready to send; use 'sendEmail' to do so.")
+}
+
+#FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+# >> sendEmail <<
+#______________________________________________________________________________
+#' Send an email (for now, just from John's Gmail account)
+#'
+#' Requires running 'setupEmail' first. This function is called after some
+#' other code runs, or whenever you like.
+#'
+#' @param pw John's secret password
+#' @param subj A string with the desired subject line
+#'
+#' @return Nothing. It just sends the Email
+#' @export
+sendEmail <- function(subj, msg){
+  #' @import blastula
+  #' @import keyring
+  txtmsg <- compose_email(
+    body=md(c(
+      msg
+    ))
+  )
+  txtmsg %>%
+    smtp_send(
+      from="johninsepdx@gmail.com",
+      to = "johninsepdx@gmail.com",
+      subject = subj,
+      credentials = creds_key(id="gmail")
+    )
+}
+
---
+++ emailExpts.R
@@ -0,0 +1,32 @@
+library(blastula)
+txtmsg <- compose_email(
+  body=md(c(
+    msg
+  ))
+)
+msg=" Up Yours"
+create_smtp_creds_key(
+  id="gmail",
+  user="johninsepdx@gmail.com",
+  provider="gmail"
+)
+
+txtmsg %>%
+  smtp_send(
+    from="johninsepdx@gmail.com",
+    to = "johninsepdx@gmail.com",
+    subject = "--- Your Analysis Is Done ---",
+    credentials = creds_key(id="gmail")
+  )
+#After executing the smtp_send code, you are asked for your
+# gmail password, which is
+#          2s$uYpL$e#qT%WP5d!^jj0RLo28aI7
+
+#It would be nice to find a way to securely store this. O
+# think it's possible using a 'keyring' package 'key_set'
+# function, of which there are several .
+
+# maybe something like this, but it's not clear how to use it....
+keyring::key_set_with_value(service = "gmail",
+                            username = "johninsepdx@gmail.com",
+                            password = "2s$uYpL$e#qT%WP5d!^jj0RLo28aI7")

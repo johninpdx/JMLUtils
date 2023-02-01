c3335ffa07a50571b4ba967cc7c8d268af603e21
johninpdx <jlight@ori.org>
2023-02-01 12:29:06 -0800

    Adds code for scoring PSQI

--- R/ScoringFuncs.R
+++ R/ScoringFuncs.R
@@ -32,25 +32,51 @@
 #'         All columns are converted to 'numeric'.
 #' @export
 PSQIScore <- function(inputTbl, wv = ""){
+#' @import glue
 #' @import tidyverse
 #' @import hms
+#'
+  # Assign raw item values, first setting NAs to 0 unless the whole row
+  #    should be missing (subject did not complete survey):
+  xx <- dim(inputTbl)
+  PSQ.items <- fixPSQNA(inputTbl)
+  #Makes scoring code compatible with any input as long as the
+  #  variables are all included and in the right order.
+  names(PSQ.items)[2] <- "ps_q1"
+  names(PSQ.items)[3] <- "ps_q2"
+  names(PSQ.items)[4] <- "ps_q3"
+  names(PSQ.items)[5] <- "ps_q4"
+  names(PSQ.items)[6] <- "ps_q5a"
+  names(PSQ.items)[7] <- "ps_q5b"
+  names(PSQ.items)[8] <- "ps_q5c"
+  names(PSQ.items)[9] <- "ps_q5d"
+  names(PSQ.items)[10] <- "ps_q5e"
+  names(PSQ.items)[11] <- "ps_q5f"
+  names(PSQ.items)[12] <- "ps_q5g"
+  names(PSQ.items)[13] <- "ps_q5h"
+  names(PSQ.items)[14] <- "ps_q5i"
+  names(PSQ.items)[15] <- "ps_q5j"
+  names(PSQ.items)[16] <- "ps_q6"
+  names(PSQ.items)[17] <- "ps_q7"
+  names(PSQ.items)[18] <- "ps_q8"
+  names(PSQ.items)[19] <- "ps_q9"
   #Check that the input is OK (as far as possible)
-  if(class(inputTbl[[4]])[1] != "hms" | class(inputTbl[[4]])[2] != "difftime") {
+  if(class(PSQ.items[[4]])[1] != "hms" | class(PSQ.items[[4]])[2] != "difftime") {
     cat("2nd column of input tbl is not class hms/difftime; aborting...")
     stop()
   }
 
-  if(class(inputTbl[[4]])[1] != "hms" | class(inputTbl[[4]])[2] != "difftime") {
+  if(class(PSQ.items[[4]])[1] != "hms" | class(PSQ.items[[4]])[2] != "difftime") {
     cat("4th column of input tbl is not class hms/difftime; aborting...")
     stop()
   }
 
-  if (length(names(inputTbl)) != 19) {
+  if (length(names(PSQ.items)) != 19) {
     cat("PSQIScore requires a 19 column tbl as input; aborting...")
     stop()
   }
 
-  if (all(unlist(lapply(lapply(inputTbl, class)[c(3,5:19)],
+  if (all(unlist(lapply(lapply(PSQ.items, class)[c(3,5:19)],
             function(x){"double" %in% x | "numeric" %in% x}))) == T){
     #Just keep going
   } else {
@@ -58,38 +84,11 @@ PSQIScore <- function(inputTbl, wv = ""){
     stop()
   }
 
-  if(any(is.na(inputTbl[,c(3,5:19)] ))){
-    cat("Must set any NAs to 0 in all numeric cols of input table; aborting...")
-    stop()
-  }
-
-  if(any(is.na(inputTbl[2,4]))){
-    cat("Either q2 (time went to bed) or q3 (hr actual sleep) is missing; aborting...")
-    stop()
-  }
-
-  # Assign raw item values:
-  PSQ.items <- inputTbl
-        #Makes scoring code compatible with any input as long as the
-        #  variables are all included and in the right order.
-        names(PSQ.items)[2] <- "ps_q1"
-        names(PSQ.items)[3] <- "ps_q2"
-        names(PSQ.items)[4] <- "ps_q3"
-        names(PSQ.items)[5] <- "ps_q4"
-        names(PSQ.items)[6] <- "ps_q5a"
-        names(PSQ.items)[7] <- "ps_q5b"
-        names(PSQ.items)[8] <- "ps_q5c"
-        names(PSQ.items)[9] <- "ps_q5d"
-        names(PSQ.items)[10] <- "ps_q5e"
-        names(PSQ.items)[11] <- "ps_q5f"
-        names(PSQ.items)[12] <- "ps_q5g"
-        names(PSQ.items)[13] <- "ps_q5h"
-        names(PSQ.items)[14] <- "ps_q5i"
-        names(PSQ.items)[15] <- "ps_q5j"
-        names(PSQ.items)[16] <- "ps_q6"
-        names(PSQ.items)[17] <- "ps_q7"
-        names(PSQ.items)[18] <- "ps_q8"
-        names(PSQ.items)[19] <- "ps_q9"
+  # Preprocess the items:
+  # If ps_q2 is missing, then the whole row needs to be missing, because it
+  #   means that the subject did not complete the survey.
+  # Otherwise, we set any items from PS_q5a on to be 0, as an NA simply means
+  #    that the question did not contribute to a "poor sleep" score.
 
   #Score the items
  PSQ.scales <- PSQ.items %>%
@@ -170,3 +169,49 @@ PSQIScore <- function(inputTbl, wv = ""){
 return(PSQ.scales)
 
 }
+
+# FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+# >> fixPSQNA <<
+#______________________________________________________________________________
+#' Internal function used by PSQIScore
+#'
+#' @param inTb A tbl_df with SSID, then all raw PSQI numeric items in order.\cr
+#'      Hence it is N x 19 (SSID + 18 PSQI items).\cr
+#'      inTb cols 2 & 4 must be of type c('hms', 'difftime' )\cr
+#'      inTb cols 3 and 5:19 must include either 'double' or 'numeric'
+#'         in their class.\cr
+#'      intTb col 1 (the ID column), the PSQI ID is normally some numeric
+#'      but can be anything.\cr
+#'      The 18 input items (ignoring SSID, the first column) must be
+#'      included in this order, though they can be named anything:\cr
+#'      - psq1\cr
+#'      - psq2\cr
+#'      - psq3\cr
+#'      - psq4\cr
+#'      - psq5a to psq5j (10 items)\cr
+#'      - psq6\cr
+#'      - psq7\cr
+#'      - psq8\cr
+#'      - psq9\cr
+#'
+#' @return A tbl_df with the same format as the input tbl_df, only with NAs
+#'      re-scored to 0's, as appropriate.
+#'
+fixPSQNA <- function(inTb){
+  for(i in 1:nrow(inTb)){
+    if(is.na(inTb[i,2])){ #If q2 missing, everything is missing
+      for(j in 2:ncol(inTb)){
+        inTb[i,j] <- NA
+      } #end j loop
+    } #end if
+    else {
+      #Set NAs to 0
+      for(j in 5:ncol(inTb)){
+        if(is.na(inTb[i,j])) {inTb[i,j] <- 0}
+      } #end j loop
+    } #end else
+  } #end i loop
+  return(inTb)
+}
+
+

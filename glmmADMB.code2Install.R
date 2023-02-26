20d4390d7d34a458fe7a1f683c10ba7237edbd87
johninpdx <jlight@ori.org>
2023-02-25 16:47:44 -0800

    v.1.3.0 Adds function 'install.glmmADMB'

---
+++ glmmADMB.code2Install.R
@@ -0,0 +1,5 @@
+install.packages("R2admb")
+install.packages("glmmADMB", 
+                 repos=c("http://glmmadmb.r-forge.r-project.org/repos",
+                         getOption("repos")),
+                 type="source")
